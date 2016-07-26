06e662973dcf15cfea0f43cf06b3a32e8561e404
Dajne <dajnewin@gmail.com>
2016-07-27 11:59:59 +1200

    Initial Commit and base hello world example

---
+++ src/main/java/nz/co/lolnet/api/Mercury/Main.java
@@ -0,0 +1,21 @@
+package nz.co.lolnet.api.Mercury;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Created by d_win on 19/07/2016.
+ */
+@Path("home")
+public class Main {
+
+    @GET
+    @Path("hello")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String helloWorld() {
+        return "Hello, world!";
+    }
+
+}
